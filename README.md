04d15832bdbd34a89e920a42f03d2b520d497e98
Rafael Soriano <rsoriano.insti@gmail.com>
2023-02-11 21:53:32 +0100

    2

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ Para poner un fondo svg:
 Para situar la x de cierre:
 [Alineación vertical de objetos](https://www.w3schools.com/cssref/pr_pos_vertical-align.asp)
 [FontAwesome](https://fontawesome.com/icons?d=gallery&q=close&m=free)
+
+# Destacable (en el Javascript)
+Para cerrar una ventana modal:
+**Haciendo clic en la x de la ventana**
+*modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));*
+**Haciendo clic fuera de la ventana**
+*window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));*
+Captura el evento clic al más alto nivel (window), y en las propiedades del evento comprueba donde hemos hecho clic. Si es en el overlay (modal) cierra la ventana. Si es dentro de la ventana, no.
+
+[W3Schools - Prevent Default](https://www.w3schools.com/jsref/event_preventdefault.asp)
+e.preventDefault() ---> El comportamiento por defecto de una form, es enviar los datos al servidor con el botón submit. Después, automáticamente se recarga la página. Para evitarlo, utilizamos este método.
+
+Validar que la URL es correcta
+**Links interesantes**
+[Mozilla - Regular Expressions](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)
+[RegEx Playground Tool](https://regexr.com/) para probar nuestras expresiones regulares
+
+**Uso habitual de LOCALSTORAGE**
+localStorage.setItem('myCat', 'Tom');
+const cat = localStorage.getItem('myCat');
+localStorage.removeItem('myCat');
+localStorage.clear();
+**Además**, cuando enviamos información a LOCALSTORAGE, igual que cuando enviamos información a un servidor, debe ser un string. Por tanto, tendremos que usar JSON.stringify.
+Para el proceso inverso (de LOCALSTORAGE al script js) usaremos JSON.parse
---
+++ script.js
@@ -0,0 +1,82 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl =document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+let bookmarks = [];
+
+// Show modal, focus on input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+// Modal event listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // Valid
+    return true;
+}
+
+// Fetch bookmarks from localStorage
+function fetchBookmarks() {
+    // Get bookmarks from localStorage if available
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name: 'Rafael Soriano',
+                url: 'http://pseidu.github.io'
+            }
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    console.log(bookmarks);
+}
+
+// Handle data from Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    };
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    };
+    const bookmark = {
+        name: nameValue,
+        url: urlValue
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On load fetch bookmarks
+fetchBookmarks();
